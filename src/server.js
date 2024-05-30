310f12d4786754d827626bccf62169edcfeb0c7e
olena-semenenko <elen.kinakh@gmail.com>
2024-05-30 16:29:16 +0200

    fixed

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import pino from 'pino-http';
 import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
+import mongoose from 'mongoose';
 
 const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
@@ -23,13 +24,13 @@ export const setupServer = () => {
     try {
       const contacts = await getAllContacts();
       res.status(200).json({
-        status: 'success',
+        status: 200,
         message: 'Successfully found contacts!',
         data: contacts,
       });
     } catch (error) {
       res.status(500).json({
-        status: 'error',
+        status: 500,
         message: 'Failed to retrieve contacts',
         error: error.message,
       });
@@ -39,16 +40,28 @@ export const setupServer = () => {
   app.get('/contacts/:contactsId', async (req, res) => {
     const contactId = req.params.contactsId;
     try {
+      if (!mongoose.isValidObjectId(contactId)) {
+        return res.status(404).json({
+          status: 404,
+          message: `Id ${contactId} is not valid`,
+        });
+      }
       const contact = await getContactById(contactId);
+      if (!contact) {
+        return res.status(404).json({
+          status: 404,
+          message: `Contact with id ${contactId} not found!`,
+        });
+      }
       res.status(200).json({
-        status: 'success',
+        status: 200,
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
     } catch (error) {
       res.status(404).json({
-        status: 'error',
-        message: `Contact with id ${contactId}! not found!`,
+        status: 404,
+        message: `Something went wrong!`,
         error: error.message,
       });
     }
