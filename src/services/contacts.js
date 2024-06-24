a5c35d71cd5ed7359dd2573a4d75cd82f5bbbe3e
olena-semenenko <elen.kinakh@gmail.com>
2024-06-24 16:59:49 +0200

    failed filter

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -11,36 +11,36 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
   // console.log(filter);
-  const totalItems = await ContactsCollection.countDocuments();
-  const contacts = await ContactsCollection.find()
-    .skip(skip)
-    .limit(limit)
-    .sort({
-      [sortBy]: sortOrder,
-    })
-    .exec();
+  // const totalItems = await ContactsCollection.countDocuments();
+  // const contacts = await ContactsCollection.find()
+  //   .skip(skip)
+  //   .limit(limit)
+  //   .sort({
+  //     [sortBy]: sortOrder,
+  //   })
+  //   .exec();
 
   // filter
-  // const contactsFilters = ContactsCollection.find();
+  const contactsFilters = ContactsCollection.find();
 
-  // if (filter.type) {
-  //   contactsFilters.where('type').equals(filter.type);
-  // }
-  // if (filter.isFavourite) {
-  //   contactsFilters.where('isFavourite').equals(filter.isFavourite);
-  // }
+  if (filter.type) {
+    contactsFilters.where('type').equals(filter.type);
+  }
+  if (filter.isFavourite) {
+    contactsFilters.where('isFavourite').equals(filter.isFavourite);
+  }
 
-  // const [totalItems, contacts] = await Promise.all([
-  //   ContactsCollection.find().merge(contactsFilters).countDocuments(),
-  //   ContactsCollection.find()
-  //     .merge(contactsFilters)
-  //     .skip(skip)
-  //     .limit(limit)
-  //     .sort({
-  //       [sortBy]: sortOrder,
-  //     })
-  //     .exec(),
-  // ]);
+  const [totalItems, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactsFilters).countDocuments(),
+    ContactsCollection.find()
+      .merge(contactsFilters)
+      .skip(skip)
+      .limit(limit)
+      .sort({
+        [sortBy]: sortOrder,
+      })
+      .exec(),
+  ]);
 
   const paginationData = calculatePaginationData(page, perPage, totalItems);
 
