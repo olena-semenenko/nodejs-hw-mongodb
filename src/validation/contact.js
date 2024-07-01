620a9272cce4acbbd4901615294e764810f25c14
olena-semenenko <elen.kinakh@gmail.com>
2024-07-01 21:08:33 +0200

    fixed

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -15,7 +15,7 @@ export const contactCreateValidationSchema = Joi.object({
   }),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
 export const contactUpdateValidationSchema = Joi.object({
