1eb3a0f4da326bba3d0e53d8d9b56a508732e73d
olena-semenenko <elen.kinakh@gmail.com>
2024-07-02 20:00:08 +0200

    fixed 1.1

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -10,7 +10,7 @@ import { setupSession } from '../utils/setupSession.js';
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
-  res.json({
+  res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
     data: user,
